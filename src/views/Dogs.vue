a6afb09b8a5beff0a78595fe25f11d72260a7715
terance98 <thomasterance98@gmail.com>
2020-03-07 16:39:13 +0530

    Initial commit

---
+++ src/views/Dogs.vue
@@ -0,0 +1,16 @@
+<template>
+  <PetTable :pets="dogs" species="Dogs" />
+ 
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["dogs"])
+  }
+};
+</script>
