a3404f2f33d6fce62b5fffe5429b5c4621d3cb7d
terance98 <thomasterance98@gmail.com>
2020-03-07 16:48:16 +0530

    Fixed the error while rendering Dogs

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,11 +1,14 @@
 <template>
   <PetTable :pets="dogs" species="Dogs" />
- 
 </template>
 
 <script>
 import { mapState } from "vuex";
+import PetTable from "@/components/PetTable.vue"
 export default {
+  components: {
+    PetTable
+  },
   data() {
     return {};
   },
